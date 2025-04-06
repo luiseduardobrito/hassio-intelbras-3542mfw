0407e059d6dd1c37f9d67431fe635f2d979b5923
Luis Brito <luiseduardo14@gmail.com>
2025-04-06 20:16:47 -0300

    fix: deprecated method

--- __init__.py
+++ __init__.py
@@ -2,22 +2,20 @@
 
 from .const import DOMAIN
 
-async def async_setup(hass, config):
+async def async_setup(_hass, _config):
     """Set up the integration from YAML (not used, but required)."""
     return True
 
 async def async_setup_entry(hass, entry):
     """Set up Intelbras 3542 MFW from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    # Forward the setup to both sensor and camera platforms and await their completion
-    result_sensor = await hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    result_camera = await hass.config_entries.async_forward_entry_setup(entry, "camera")
-    return result_sensor and result_camera
+    # Forward the setup to both sensor and camera platforms using the new API
+    result = await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "camera"])
+    return result
 
 async def async_unload_entry(hass, entry):
     """Unload Intelbras 3542 MFW config entry."""
-    unload_sensor = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    unload_camera = await hass.config_entries.async_forward_entry_unload(entry, "camera")
-    if unload_sensor and unload_camera:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor", "camera"])
+    if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_sensor and unload_camera
+    return unload_ok
