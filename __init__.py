986a760b861291ce3cb5f1f769262e5b32bc2864
Luis Brito <luiseduardo14@gmail.com>
2025-04-06 20:25:24 -0300

    fix: setup

--- __init__.py
+++ __init__.py
@@ -9,15 +9,21 @@ async def async_setup(hass, config):
 async def async_setup_entry(hass, entry):
     """Set up Intelbras 3542 MFW from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    # Forward the setup to both sensor and camera platforms and await their completion
-    result_sensor = await hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    result_camera = await hass.config_entries.async_forward_entry_setup(entry, "camera")
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor"),
+        hass.config_entries.async_forward_entry_setup(entry, "camer"),
+    )
     return True
 
 async def async_unload_entry(hass, entry):
     """Unload Intelbras 3542 MFW config entry."""
-    unload_sensor = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    unload_camera = await hass.config_entries.async_forward_entry_unload(entry, "camera")
-    if unload_sensor and unload_camera:
+    unload_sensor_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    unload_camera_ok = await hass.config_entries.async_forward_entry_unload(entry, "camera")
+
+    if unload_sensor_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_sensor and unload_camera
+
+    if unload_camera_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+        
+    return True
