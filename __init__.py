7e9fe16d766c708f6fa232fef83ca7deb9d099d2
Luis Brito <luiseduardo14@gmail.com>
2025-04-21 20:20:01 -0300

    feat: webhook

--- __init__.py
+++ __init__.py
@@ -1,14 +1,31 @@
 """The Intelbras 3542 MFW integration."""
 
+import json
 from .const import DOMAIN
 from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.components.webhook import async_register as async_register_webhook 
 
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.CAMERA, Platform.BUTTON]
 
 async def async_setup(hass, config):
     """Set up the integration from YAML (not used, but required)."""
+    webhook_id = "intelbras_3542_mfw_webhook"
+    async_register_webhook(
+        hass,
+        webhook_id,
+        handle_webhook_event,
+    )
     return True
+
+async def handle_webhook_event(hass: HomeAssistant, webhook_id: str, request):
+    """Handle the incoming webhook event."""
+    payload = await request.json()
+    # Optionally, log the payload for debugging
+    hass.logger.info(f"Received webhook payload: {json.dumps(payload)}")
+    # Fire a custom event with the payload data
+    hass.bus.async_fire("intelbras_3542_mfw_webhook", payload)
 
 async def async_setup_entry(hass, entry):
     """Set up Intelbras 3542 MFW from a config entry."""
